098f8d67a15b46a9567ef5f19333dc763127608a
JalalMitali <JalalMitaliShopping@gmail.com>
2022-10-02 14:57:31 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/*.{js,ts,jsx,tsx}",
+    "./components/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.slate,
+        green: colors.emerald,
+        purple: colors.violet,
+        yellow: colors.amber,
+        pink: colors.fuchsia,
+        amber: colors.amber,
+      },
+      minHeight: {
+        appBar: '6vh',
+      },
+      minWidth: {
+        appBar: '100%',
+      },
+    },
+  },
+  plugins: [],
+}
