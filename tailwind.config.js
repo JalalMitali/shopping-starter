7c25638ff06795ee23b2c4d02d062636ddc51121
JalalMitali <JalalMitaliShopping@gmail.com>
2022-10-03 15:16:59 +0100

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,16 @@ module.exports = {
       minWidth: {
         appBar: '100%',
       },
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
